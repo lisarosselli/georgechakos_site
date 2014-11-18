d5e834717498821aab1a4b38ba1d9414faad72ea
lisarosselli <studio1809@gmail.com>
2014-11-17 20:00:31 -0600

    georges order and art selections

--- js/main.js
+++ js/main.js
@@ -4,11 +4,12 @@ console.log("main!");
 var gc = {};
 
 gc.g0 = {
-		name: "Western",
+		name: "Rocky Mtn. Art",
 		images: [
-			{name:"A Challenge", 
-				thumb:"img/art/thumbs/a_challenge.jpg", 
-				src:"img/art/a_challenge.jpg"},
+			{name:"Fall River",
+				thumb:"img/art/thumbs/michigan_river.jpg",
+				src:"img/art/michigan_river.jpg"
+				},
 			{name:"Cowboys at Maroon Bells", 
 				thumb:"img/art/thumbs/cowboys_at_maroon_bells.jpg", 
 				src:"img/art/cowboys_at_maroon_bells.jpg"},
@@ -29,17 +30,9 @@ gc.g0 = {
 				thumb:"img/art/thumbs/Hes_Too_Big.jpg",
 				src:"img/art/Hes_Too_Big.jpg"
 				},
-			{name:"King of Beasts",
-				thumb:"img/art/thumbs/King_of_Beasts.jpg",
-				src:"img/art/King_of_Beasts.jpg"
-				},
 			{name:"Looking for Strays",
 				thumb:"img/art/thumbs/Looking_for_Strays.jpg",
 				src:"img/art/Looking_for_Strays.jpg"
-				},
-			{name:"Lions 2",
-				thumb:"img/art/thumbs/lions2.jpg",
-				src:"img/art/lions2.jpg"
 				},
 			{name:"Maroon Bells Stream",
 				thumb:"img/art/thumbs/Maroon_Bells_Stream.jpg",
@@ -61,9 +54,16 @@ gc.g0 = {
 				thumb:"img/art/thumbs/Mountain_Falls.jpg",
 				src:"img/art/Mountain_Falls.jpg"
 				},
+			{name:"A Challenge", 
+				thumb:"img/art/thumbs/a_challenge.jpg", 
+				src:"img/art/a_challenge.jpg"},
 			{name:"Mountain Lake",
 				thumb:"img/art/thumbs/Mountain_Lake.jpg",
 				src:"img/art/Mountain_Lake.jpg"
+				},
+			{name:"Steam Engine",
+				thumb:"img/art/thumbs/steam_engine.jpg",
+				src:"img/art/steam_engine.jpg"
 				},
 			{name:"Mountain Moose",
 				thumb:"img/art/thumbs/Mountain_Moose.jpg",
@@ -72,10 +72,6 @@ gc.g0 = {
 			{name:"Night Moose",
 				thumb:"img/art/thumbs/Night_Moose.jpg",
 				src:"img/art/Night_Moose.jpg"
-				},
-			{name:"Oryx 2",
-				thumb:"img/art/thumbs/oryx2.jpg",
-				src:"img/art/oryx2.jpg"
 				},
 			{name:"Rocky Mountain Elk",
 				thumb:"img/art/thumbs/Rocky_Mountain_Elk.jpg",
@@ -127,14 +123,6 @@ gc.g1 = {
 				thumb:"img/art/thumbs/Autum_Falls.jpg",
 				src:"img/art/Autum_Falls.jpg"
 				},
-			{name:"Michigan River",
-				thumb:"img/art/thumbs/michigan_river.jpg",
-				src:"img/art/michigan_river.jpg"
-				},
-			{name:"Trees in Snow",
-				thumb:"img/art/thumbs/Trees_in_Snow.jpg",
-				src:"img/art/Trees_in_Snow.jpg"
-				},
 			{name:"Winter Light",
 				thumb:"img/art/thumbs/Winter_Light.jpg",
 				src:"img/art/Winter_Light.jpg"
@@ -151,86 +139,100 @@ gc.g1 = {
 };
 
 gc.g2 = {
-		name: "Other",
+		name: "African",
 		images: [
-			{name:"Apple Pie",
-				thumb:"img/art/thumbs/apple_pie.jpg",
-				src:"img/art/apple_pie.jpg"
+			{name:"King of Beasts",
+				thumb:"img/art/thumbs/King_of_Beasts.jpg",
+				src:"img/art/King_of_Beasts.jpg"
 				},
-			{name: "Red Table",
-				thumb:"img/art/thumbs/red_table.jpg",
-				src:"img/art/red_table.jpg"
+			{name:"Lions!",
+				thumb:"img/art/thumbs/lions2.jpg",
+				src:"img/art/lions2.jpg"
 				},
-			{name:"m001",
-				thumb:"img/art/thumbs/m001.jpg",
-				src:"img/art/m001.jpg"
-				},
-			{name:"m002",
-				thumb:"img/art/thumbs/m002.jpg",
-				src:"img/art/m002.jpg"
-				},
-			{name:"m003",
-				thumb:"img/art/thumbs/m003.jpg",
-				src:"img/art/m003.jpg"
-				},
-			{name:"m004",
-				thumb:"img/art/thumbs/m004.jpg",
-				src:"img/art/m004.jpg"
-				},
-			{name:"m005",
-				thumb:"img/art/thumbs/m005.jpg",
-				src:"img/art/m005.jpg"
-				},
-			{name:"m006",
-				thumb:"img/art/thumbs/m006.jpg",
-				src:"img/art/m006.jpg"
-				},
-			{name:"m007",
-				thumb:"img/art/thumbs/m007.jpg",
-				src:"img/art/m007.jpg"
-				},
-			{name:"m009",
-				thumb:"img/art/thumbs/m009.jpg",
-				src:"img/art/m009.jpg"
-				},
-			{name:"m010",
-				thumb:"img/art/thumbs/m010.jpg",
-				src:"img/art/m010.jpg"
-				},
-			{name:"m011",
-				thumb:"img/art/thumbs/m011.jpg",
-				src:"img/art/m011.jpg"
-				},
-			{name:"New York Winter",
-				thumb:"img/art/thumbs/new_york_winter.jpg",
-				src:"img/art/new_york_winter.jpg"
-				},
-			{name:"Red Table",
-				thumb:"img/art/thumbs/red_table.jpg",
-				src:"img/art/red_table.jpg"
-				},
-			{name:"Shadow Patterns",
-				thumb:"img/art/thumbs/shadow_patterns.jpg",
-				src:"img/art/shadow_patterns.jpg"
-				},
-			{name:"Spring Falls",
-				thumb:"img/art/thumbs/spring_falls.jpg",
-				src:"img/art/spring_falls.jpg"
-				},
-			{name:"Spring Shadows",
-				thumb:"img/art/thumbs/spring_shadows.jpg",
-				src:"img/art/spring_shadows.jpg"
-				},
-			{name:"Steam Engine",
-				thumb:"img/art/thumbs/steam_engine.jpg",
-				src:"img/art/steam_engine.jpg"
-				},
-			{name:"Winter Forest",
-				thumb:"img/art/thumbs/winter_forest.jpg",
-				src:"img/art/winter_forest.jpg"
+			{name:"Oryx 2",
+				thumb:"img/art/thumbs/oryx2.jpg",
+				src:"img/art/oryx2.jpg"
 				}
 		]
 };
+
+gc.g3 = {
+	name:"Other",
+	images: [
+		{name:"Apple Pie",
+				thumb:"img/art/thumbs/apple_pie.jpg",
+				src:"img/art/apple_pie.jpg"
+				},
+		{name: "Red Table",
+			thumb:"img/art/thumbs/red_table.jpg",
+			src:"img/art/red_table.jpg"
+			},
+		{name:"m001",
+			thumb:"img/art/thumbs/m001.jpg",
+			src:"img/art/m001.jpg"
+			},
+		{name:"m002",
+			thumb:"img/art/thumbs/m002.jpg",
+			src:"img/art/m002.jpg"
+			},
+		{name:"m003",
+			thumb:"img/art/thumbs/m003.jpg",
+			src:"img/art/m003.jpg"
+			},
+		{name:"m004",
+			thumb:"img/art/thumbs/m004.jpg",
+			src:"img/art/m004.jpg"
+			},
+		{name:"m005",
+			thumb:"img/art/thumbs/m005.jpg",
+			src:"img/art/m005.jpg"
+			},
+		{name:"m006",
+			thumb:"img/art/thumbs/m006.jpg",
+			src:"img/art/m006.jpg"
+			},
+		{name:"m007",
+			thumb:"img/art/thumbs/m007.jpg",
+			src:"img/art/m007.jpg"
+			},
+		{name:"m009",
+			thumb:"img/art/thumbs/m009.jpg",
+			src:"img/art/m009.jpg"
+			},
+		{name:"m010",
+			thumb:"img/art/thumbs/m010.jpg",
+			src:"img/art/m010.jpg"
+			},
+		{name:"m011",
+			thumb:"img/art/thumbs/m011.jpg",
+			src:"img/art/m011.jpg"
+			},
+		{name:"New York Winter",
+			thumb:"img/art/thumbs/new_york_winter.jpg",
+			src:"img/art/new_york_winter.jpg"
+			},
+		{name:"Red Table",
+			thumb:"img/art/thumbs/red_table.jpg",
+			src:"img/art/red_table.jpg"
+			},
+		{name:"Shadow Patterns",
+			thumb:"img/art/thumbs/shadow_patterns.jpg",
+			src:"img/art/shadow_patterns.jpg"
+			},
+		{name:"Spring Falls",
+			thumb:"img/art/thumbs/spring_falls.jpg",
+			src:"img/art/spring_falls.jpg"
+			},
+		{name:"Spring Shadows",
+			thumb:"img/art/thumbs/spring_shadows.jpg",
+			src:"img/art/spring_shadows.jpg"
+			},
+		{name:"Winter Forest",
+			thumb:"img/art/thumbs/winter_forest.jpg",
+			src:"img/art/winter_forest.jpg"
+			}
+	]
+}
 
 gc.galleries = [
 			{galObj: gc.g0, galClass: ".g0"},
